e69543ead5b6c3bd0920caabd5f861850deb52a4
sadik182 <sadikurrahman728@gmail.com>
2022-07-01 01:51:54 +0600

    Header and Banner Section

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faGoogle, faFacebook, faGithub, faTwitter, faFacebookF, faLinkedinIn, faGithubAlt} from '@fortawesome/free-brands-svg-icons'
+import { faCoffee} from '@fortawesome/free-solid-svg-icons'
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
 import bannerImg from '../../images/sadik.png';
+import './Banner.css';
 
 const Banner = () => {
     return (
         <Container>
             <div className='row d-flex justify-content-center align-items-center'>
             <div className="col-lg-6 col-md-6 col-sm-12">
-                <h1>This is Md. Sadikur Rahman</h1>
-                <h3>Jr. Software Engineer</h3>
-                <h3>I am working on MERN Stack</h3>
-                <ul>
-                    <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
-                </ul>
+                <h1 className="fw-bold fs-1">This is Md. Sadikur Rahman</h1>
+                <h3 className="fw-bold fs-3">Jr. Software Engineer</h3>
+                <h3>I am working as a MERN Stack Developer</h3>
+                <div className="d-flex icon fs-5 py-3">
+                    <a href="#"><FontAwesomeIcon icon={faFacebookF}/></a>
+                    <a href="#"><FontAwesomeIcon icon={faGithub}/></a>
+                    <a href="#"><FontAwesomeIcon icon={faLinkedinIn}/></a>
+                    <a href="#"><FontAwesomeIcon icon={faTwitter}/></a>
+                </div>
+                <div className="d-flex py-4">
+                <Button href="" variant='primary' size='lg' className="me-4">Hire Me</Button>
+                <Button href="" variant='primary' size='lg'>Download CV</Button>
+                </div>
             </div>
             <div className="col-lg-6 col-md-6 col-sm-12">
                <div className='py-5'>
